7a067ccf6f19c3bc5b366671689e82d2602a47de
Yevgeniy Polonskiy <Yevgeniy.Polonskiy@oracle.com>
2022-01-26 18:57:37 -0800

    Updated documentation for 5.1.3.

--- styles/docfx.js
+++ styles/docfx.js
@@ -990,7 +990,7 @@ $(function () {
     }
 
     function readTabsQueryStringParam() {
-      var qs = parseQueryString(window.location.search);
+      var qs = parseQueryString();
       var t = qs.tabs;
       if (t === undefined || t === '') {
         return [];
@@ -999,7 +999,7 @@ $(function () {
     }
 
     function updateTabsQueryStringParam(state) {
-      var qs = parseQueryString(window.location.search);
+      var qs = parseQueryString();
       qs.tabs = state.selectedTabs.join();
       var url = location.protocol + "//" + location.host + location.pathname + "?" + toQueryString(qs) + location.hash;
       if (location.href === url) {
